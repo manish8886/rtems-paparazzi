a837e1047b11ec241707752b80166d533fee7255
dell <manish@ubuntu.ubuntu-domain>
2013-07-02 16:00:30 -0400

    addin pap files to rtems

---
+++ includes/device_names.h
@@ -0,0 +1,14 @@
+/*
+ * device_names.h
+ *
+ *  Created on: Jun 8, 2013
+ *      Author: manish
+ */
+
+#ifndef DEVICE_NAMES_H_
+#define DEVICE_NAMES_H_
+
+#define gps_device1      "/dev/gps1"
+#define GPS_DEVICE_NO     1
+
+#endif /* DEVICE_NAMES_H_ */
